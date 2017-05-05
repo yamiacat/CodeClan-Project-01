0ae44b84d815d041580bebc9e4005396288be147
Matt Rose <yamiacat@gmail.com>
2017-05-05 20:58:57 +0100

    Add album class with save, delete all functions

---
+++ tunes_matter/models/album.rb
@@ -0,0 +1,30 @@
+require_relative("../db/sql_runner.rb")
+require_relative("./artist.rb")
+require_relative("./edition.rb")
+
+class Album
+
+  attr_reader :id
+  attr_accessor :artist_id, :title, :genre, :original_release_year
+
+  def initialize(params)
+    @id = params['id'].to_i()
+    @artist_id = params['artist_id'].to_i()
+    @title = params['title']
+    @genre = params['genre']
+    @original_release_year = params['original_release_year'].to_i()
+  end
+
+  def save()
+    sql = "INSERT INTO albums (artist_id, title, genre, original_release_year) VALUES (#{@artist_id}, '#{@title}', '#{@genre}', #{@original_release_year}) RETURNING id;"
+    returned_result = SqlRunner.run(sql)
+    @id = returned_result.first()['id'].to_i()
+  end
+
+  def Artist.delete_all()
+    sql = "DELETE FROM albums;"
+    SqlRunner.run(sql)
+  end
+
+
+end
--- tunes_matter/models/artist.rb
+++ tunes_matter/models/artist.rb
@@ -8,14 +8,14 @@ class Artist
   attr_accessor :name
 
   def initialize(params)
-    @id = params['id'].to_i
+    @id = params['id'].to_i()
     @name = params['name']
   end
 
   def save()
     sql = "INSERT INTO artists (name) VALUES ('#{@name}') RETURNING id;"
     returned_result = SqlRunner.run(sql)
-    @id = returned_result.first()['id'].to_i
+    @id = returned_result.first()['id'].to_i()
   end
 
   def Artist.delete_all()
