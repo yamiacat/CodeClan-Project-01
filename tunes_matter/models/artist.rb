292505f6306905a31bf28145b126965d0952224e
Matt Rose <yamiacat@gmail.com>
2017-05-05 20:17:10 +0100

    Add artist class with save function

---
+++ tunes_matter/models/artist.rb
@@ -0,0 +1,24 @@
+require_relative("../db/sql_runner.rb")
+require_relative("./album.rb")
+require_relative("./edition.rb")
+
+class Artist
+
+  attr_reader :id
+  attr_accessor :name
+
+  def initialize(params)
+    @id = params['id'].to_i
+    @name = params['name']
+  end
+
+  def save()
+    sql = "INSERT INTO artists (name) VALUES ('#{@name}') RETURNING id;"
+    returned_result = SqlRunner.run(sql)
+    @id = returned_result.first()['id'].to_i
+  end
+
+
+
+
+end
