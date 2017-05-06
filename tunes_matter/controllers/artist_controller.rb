82a81d1b68b8f7bf1c23e570f480e2fc6a6836dc
Matt Rose <yamiacat@gmail.com>
2017-05-06 15:11:48 +0100

    Add restful routes for Artists

--- tunes_matter/controllers/artist_controller.rb
+++ tunes_matter/controllers/artist_controller.rb
@@ -2,7 +2,52 @@ require("sinatra")
 require("sinatra/contrib/all")
 require_relative("../models/artist.rb")
 
+# INDEX - READ (all)
+
 get "/artists" do
   @artists = Artist.all()
-  erb ( :"artists/index" )
+  erb (:"artists/index")
+end
+
+#NEW - CREATE (get form)
+
+get "/artists/new" do
+  erb(:"artists/new")
+end
+
+#CREATE - CREATE (make something)
+
+post "/artists" do
+  @artist = Artist.new(params)
+  @artist.save()
+  erb(:"artists/create")
+end
+
+#SHOW - READ (one)
+get "/artists/:id" do
+  @artist = Artist.find(params[:id])
+  erb(:"artists/show")
+end
+
+#EDIT UPDATE (get form)
+
+get "/artists/:id/edit" do
+  @artist = Artist.find(params[:id])
+  erb(:"artists/edit")
+end
+
+#UPDATE UPDATE (make change)
+
+post "/artists/:id" do
+  @artist = Artist.new(params)
+  @artist.update()
+  erb(:"artists/update")
+end
+
+#DESTROY DELETE
+
+post "/artists/:id/delete" do
+  @artist = Artist.find(params[:id])
+  @artist.delete()
+  erb(:"artists/destroy")
 end
--- tunes_matter/controllers/edition_controller.rb
+++ tunes_matter/controllers/edition_controller.rb
@@ -1,2 +1,17 @@
 require("sinatra")
 require("sinatra/contrib/all")
+
+
+# INDEX - READ (all)
+
+#NEW - CREATE (get form)
+
+#CREATE - CREATE (make something)
+
+#SHOW - READ (one)
+
+#EDIT UPDATE (get form)
+
+#UPDATE UPDATE (make change)
+
+#DESTROY DELETE
