d4a47b13a262d0ad6721d42d17eef28f0b674e81
Matt Rose <yamiacat@gmail.com>
2017-05-06 12:29:34 +0100

    Add basic CSS retro styling

--- tunes_matter/app.rb
+++ tunes_matter/app.rb
@@ -1,6 +1,9 @@
 require("sinatra")
 require("sinatra/contrib/all")
-
+require_relative("controllers/album_controller.rb")
+require_relative("controllers/artist_controller.rb")
+require_relative("controllers/edition_controller.rb")
+require_relative("models/shop.rb")
 
 get '/' do
   erb( :index )
